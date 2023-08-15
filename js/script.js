be2b68a2ade5c53af85363eeba3b93b1004c4534
ayunghaw <wahyu.hidayat4546@gmail.com>
2023-08-15 17:52:23 +0700

    update source code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
 const alas = document.getElementById('alas')
 const tinggi = document.getElementById('tinggi')
-const hitung = document.getElementById('hitung')
-const output = document.getElementById('output')
+const hluas = document.getElementById('hluas')
+const hkeliling = document.getElementById('hkeliling')
+const outputl = document.getElementById('outputl')
+const outputk = document.getElementById('outputk')
 
-hitung.addEventListener('click', function() {
+// menghitung luas
+hluas.addEventListener('click', function() {
     let a = alas.value
     let t = tinggi.value
     let l = 0.5*a*t
-    output.innerHTML = `Luas segitiga dari alas ${a} cm dan tinggi ${t} cm <br> adalah ${l} cm2`
+
+    outputl.innerHTML = 
+        `L = 1/2 x a x t <br>
+        L = 1/2 x ${a} x ${t} <br>
+        L = ${l} <br><br>
+        
+        Jadi luas segitiganya adalah ${l}`
 })
+// END
+
+// menghitung keliling
+hkeliling.addEventListener('click', function() {
+    let a = sisia.value
+    let b = sisib.value
+    let c = sisic.value
+    let k = (parseInt(a))+(parseInt(b))+(parseInt(c))
+
+    outputk.innerHTML = 
+        `K = S1 + S2 +S3 <br>
+        K = ${a} + ${b} + ${c} <br>
+        K = ${k} <br><br>
+        
+        Jadi keliling segitiganya adalah ${k}`
+})
+// END
