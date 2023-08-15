451b6d5bf8d825f77cefcd036ef09afd36fb7d59
ayunghaw <wahyu.hidayat4546@gmail.com>
2023-08-15 14:50:11 +0700

    upload source code

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const alas = document.getElementById('alas')
+const tinggi = document.getElementById('tinggi')
+const hitung = document.getElementById('hitung')
+const output = document.getElementById('output')
+
+hitung.addEventListener('click', function() {
+    let a = alas.value
+    let t = tinggi.value
+    let l = 0.5*a*t
+    output.innerHTML = `Luas segitiga dari alas ${a} cm dan tinggi ${t} cm <br> adalah ${l} cm2`
+})
